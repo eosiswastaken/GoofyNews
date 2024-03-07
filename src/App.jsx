93b6c05c821e925bbb78b6e73b94123fc6e536aa
eosiswastaken <eosiswastaken@gmail.com>
2024-03-07 15:32:17 +0100

    📈analytics: add VA

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
 
   return (
     <>
+    <Analytics/>
    <div className='title'>
     <h1>Goofy News</h1>
    </div>
