e3eab4c6f604e1f10a71267096d799336316e778
RemyEroes <113907739+RemyEroes@users.noreply.github.com>
2024-03-07 16:04:45 +0100

    goofy commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,30 @@ function App() {
 
   return (
     <>
+<<<<<<< Updated upstream
     <Analytics/>
    <div className='title'>
     <h1>Goofy News</h1>
    </div>
+=======
+      <div className="container-title">
+        <div className='title is-animated unselectable'>
+          <span>G</span>
+          <span>o</span>
+          <span>o</span>
+          <span>f</span>
+          <span>y</span>
+
+          <span>N</span>
+          <span>e</span>
+          <span>w</span>
+          <span>s</span>
+        </div>
+      </div>
+      <div className="link unselectable">
+          <a href="">Devenir culturé 🧠</a>
+        </div>
+>>>>>>> Stashed changes
     </>
   )
 }
