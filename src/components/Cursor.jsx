684ac4496d6eb51c0c5e3ff2269adcd66789d895
RemyEroes <113907739+RemyEroes@users.noreply.github.com>
2024-03-09 12:11:06 +0100

    goofy custom cursor

--- src/components/Cursor.jsx
+++ src/components/Cursor.jsx
@@ -4,12 +4,24 @@ import mainMickey from '../assets/main-mickey.png';
 
 function CustomCursor() {
   const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
+  const [cursorRotation, setCursorRotation] = useState(0);
   const [cursorOnPage, setCursorOnPage] = useState(false);
 
   useEffect(() => {
+    let lastPosition = { x: 0, y: 0 };
+    let intervalId;
+
     const handleMouseMove = (event) => {
-      const { clientX: x, clientY: y } = event;
-      setCursorPosition({ x, y });
+        const { clientX: x, clientY: y } = event;
+
+        const xDiff = x - lastPosition.x;
+        // const yDiff = y - lastPosition.y;
+        const rotation = xDiff * .9;
+
+        setCursorPosition({ x, y });
+        setCursorRotation(rotation);
+
+        lastPosition = { x, y };
     };
 
     const handleMouseEnter = () => {
@@ -28,11 +40,12 @@ function CustomCursor() {
       document.removeEventListener('mousemove', handleMouseMove);
       document.removeEventListener('mouseenter', handleMouseEnter);
       document.removeEventListener('mouseleave', handleMouseLeave);
+      clearInterval(intervalId);
     };
   }, []);
 
   return (
-    <div className={`custom-cursor ${cursorOnPage ? '' : 'inactive'}`} style={{ left: cursorPosition.x, top: cursorPosition.y }}>
+    <div className={`custom-cursor ${cursorOnPage ? '' : 'inactive'}`} style={{ left: cursorPosition.x, top: cursorPosition.y, transform: `translate(-50%, -10%) rotate(${cursorRotation}deg)` }}>
      <img src={mainMickey} alt="cursor" />
     </div>
   );
